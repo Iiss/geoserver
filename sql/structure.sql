2a3b87489e7afe88c719d7baa6e8f9d7a33bbe47
Ilya Alisov <ia13@bk.ru>
2015-09-30 13:50:31 +0300

    DB structure updated

--- sql/structure.sql
+++ sql/structure.sql
@@ -1,36 +1,122 @@
-CREATE SCHEMA `geo` ;
+-- MySQL dump 10.13  Distrib 5.6.24, for Win64 (x86_64)
+--
+-- Host: localhost    Database: geo
+-- ------------------------------------------------------
+-- Server version	5.6.26-log
 
-CREATE TABLE `geo`.`sessions` (
-  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
-  `title` VARCHAR(128) NULL COMMENT '',
-  PRIMARY KEY (`id`)  COMMENT '');
-  
-CREATE TABLE `geo`.`scan_types` (
-  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
-  `title` VARCHAR(64) NULL COMMENT '',
-  PRIMARY KEY (`id`)  COMMENT '');
-  
-CREATE TABLE `geo`.`scan_requests` (
-  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
-  `cell_x` INT NULL COMMENT '',
-  `cell_y` INT NULL COMMENT '',
-  `scan_type_id` INT NOT NULL COMMENT '',
-  PRIMARY KEY (`id`)  COMMENT '');
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE `geo`.`scan_data` (
-  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
-  `cell_x` INT NULL COMMENT '',
-  `cell_y` INT NULL COMMENT '',
-  `scan_type_id` INT NOT NULL COMMENT '',
-  `value` DECIMAL(10,6) NULL COMMENT '',
-  PRIMARY KEY (`id`)  COMMENT '');session_data
-  
-CREATE TABLE `geo`.`map_data` (
-  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
-  `cell_x` INT NULL COMMENT '',
-  `cell_y` INT NULL COMMENT '',
-  `scan_type_id` INT NOT NULL COMMENT '',
-  `session_id` INT NULL COMMENT '',
-  `value` DECIMAL(10,6) NULL DEFAULT 0 COMMENT '',
-  PRIMARY KEY (`id`)  COMMENT '');
-  
+--
+-- Table structure for table `layers`
+--
+
+DROP TABLE IF EXISTS `layers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `layers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `map_data`
+--
+
+DROP TABLE IF EXISTS `map_data`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `map_data` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cell_x` int(11) DEFAULT NULL,
+  `cell_y` int(11) DEFAULT NULL,
+  `layer_id` int(11) NOT NULL,
+  `map_id` int(11) DEFAULT NULL,
+  `value` decimal(10,6) DEFAULT '0.000000',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `maps`
+--
+
+DROP TABLE IF EXISTS `maps`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `maps` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(128) DEFAULT NULL,
+  `width` int(2) DEFAULT '31',
+  `height` int(2) DEFAULT '17',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `scan_data`
+--
+
+DROP TABLE IF EXISTS `scan_data`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `scan_data` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cell_x` int(11) DEFAULT NULL,
+  `cell_y` int(11) DEFAULT NULL,
+  `scan_type_id` int(11) NOT NULL,
+  `value` decimal(10,6) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `scan_requests`
+--
+
+DROP TABLE IF EXISTS `scan_requests`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `scan_requests` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cell_x` int(11) DEFAULT NULL,
+  `cell_y` int(11) DEFAULT NULL,
+  `scan_type_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `sessions`
+--
+
+DROP TABLE IF EXISTS `sessions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sessions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `map_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-09-30 10:38:40
