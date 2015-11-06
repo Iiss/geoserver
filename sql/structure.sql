7f4f7136461a1132697c3c9e97851c5d8e3d3b80
Ilya Alisov <ia13@bk.ru>
2015-11-06 10:52:22 +0300

    sql structure changed. Foreign keys added

--- sql/structure.sql
+++ sql/structure.sql
@@ -24,9 +24,10 @@ DROP TABLE IF EXISTS `layers`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `layers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(64) DEFAULT NULL,
+  `title` varchar(45) DEFAULT NULL,
+  `layer_type` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,11 +41,17 @@ CREATE TABLE `map_data` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `cell_x` int(11) DEFAULT NULL,
   `cell_y` int(11) DEFAULT NULL,
+  `map_id` int(11) NOT NULL,
   `layer_id` int(11) NOT NULL,
-  `map_id` int(11) DEFAULT NULL,
-  `value` decimal(10,6) DEFAULT '0.000000',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+  `value_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`,`map_id`,`layer_id`,`value_id`),
+  KEY `fk_map_data_maps_idx` (`map_id`),
+  KEY `fk_map_data_layers1_idx` (`layer_id`),
+  KEY `fk_map_data_values1_idx` (`value_id`),
+  CONSTRAINT `fk_map_data_layers1` FOREIGN KEY (`layer_id`) REFERENCES `layers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_map_data_maps` FOREIGN KEY (`map_id`) REFERENCES `maps` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_map_data_values1` FOREIGN KEY (`value_id`) REFERENCES `values` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=6905 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -56,9 +63,8 @@ DROP TABLE IF EXISTS `maps`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `maps` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(128) DEFAULT NULL,
-  `width` int(2) DEFAULT '31',
-  `height` int(2) DEFAULT '17',
+  `width` int(11) DEFAULT '32',
+  `height` int(11) DEFAULT '18',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -74,10 +80,12 @@ CREATE TABLE `scan_data` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `cell_x` int(11) DEFAULT NULL,
   `cell_y` int(11) DEFAULT NULL,
-  `scan_type_id` int(11) NOT NULL,
-  `value` decimal(10,6) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `value` int(11) DEFAULT NULL,
+  `layer_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`,`layer_id`),
+  KEY `fk_scan_data_layers1_idx` (`layer_id`),
+  CONSTRAINT `fk_scan_data_layers1` FOREIGN KEY (`layer_id`) REFERENCES `layers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=211 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -91,9 +99,11 @@ CREATE TABLE `scan_requests` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `cell_x` int(11) DEFAULT NULL,
   `cell_y` int(11) DEFAULT NULL,
-  `scan_type_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `layer_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`,`layer_id`),
+  KEY `fk_scan_requests_layers1_idx` (`layer_id`),
+  CONSTRAINT `fk_scan_requests_layers1` FOREIGN KEY (`layer_id`) REFERENCES `layers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=132 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -106,6 +116,40 @@ DROP TABLE IF EXISTS `sessions`;
 CREATE TABLE `sessions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `map_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`,`map_id`),
+  KEY `fk_sessions_maps1_idx` (`map_id`),
+  CONSTRAINT `fk_sessions_maps1` FOREIGN KEY (`map_id`) REFERENCES `maps` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `storage`
+--
+
+DROP TABLE IF EXISTS `storage`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `storage` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `kern_id` int(11) DEFAULT NULL,
+  `cell_x` int(11) DEFAULT NULL,
+  `cell_y` int(11) DEFAULT NULL,
+  `rock_key` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `values`
+--
+
+DROP TABLE IF EXISTS `values`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `values` (
+  `id` int(11) NOT NULL,
+  `value` varchar(45) DEFAULT NULL,
+  `data_format` varchar(45) DEFAULT 'int',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -119,4 +163,4 @@ CREATE TABLE `sessions` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-09-30 10:38:40
+-- Dump completed on 2015-11-06 10:50:03
